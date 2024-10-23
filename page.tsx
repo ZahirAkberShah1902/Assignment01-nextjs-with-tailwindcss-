5f773cb6806d93090d1b44159479bd88869c62cc
Zahir Akber Shah <161370075+ZahirAkberShah1902@users.noreply.github.com>
2024-10-23 15:06:11 +0500

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,4 +1,14 @@
 
+
+export default function Home(){
+  return(
+<div>
+
+<h1 className="border-4 border-stone-700 m-40 p-5 text-pink-600 font-bold text-center">Hello gentleman! its my first project at nextjs using tailwind</h1>
+</div>
+  )
+}
+
 export default async function About(){
   await new Promise((resolve) => {
     setTimeout(resolve, 2000)
@@ -6,6 +16,39 @@ export default async function About(){
   return(
   <div>
   <h1  className="border-4 border-stone-700 m-40 p-5 text-pink-600 font-bold text-center">About Page</h1>
+  </div>
+    )
+  }
+
+export default function Contact(){
+    return(
+  <div>
+  <h1  className="border-4 border-stone-700 m-40 p-5 text-pink-600 font-bold text-center">Contact Page</h1>
+  </div>
+    )
+  }
+
+export default function Details(){
+    return(
+  <div>
+  <h1  className="border-4 border-stone-700 m-40 p-5 text-pink-600 font-bold text-center">For further details</h1>
+  </div>
+    )
+  }import {error} from "console"
+
+export default function Job(){
+  throw new Error ('JOb page not yet implemented')
+  return(
+  <div>
+  <h1>Job Page</h1>
+  </div>
+    )
+  }
+//nested function: job/programming
+export default function Programming(){
+    return(
+  <div>
+  <h1  className="border-4 border-stone-700 m-40 p-5 text-pink-600 font-bold text-center">Programming Page</h1>
   </div>
     )
   }
