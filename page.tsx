43354c0b96108ea8a3e845565b0dafc5cc1019bc
Zahir Akber Shah <161370075+ZahirAkberShah1902@users.noreply.github.com>
2024-10-23 15:01:57 +0500

    Add files via upload
    about page.tsx

--- page.tsx
+++ page.tsx
@@ -1,9 +1,11 @@
 
-
-export default function Programming(){
-    return(
+export default async function About(){
+  await new Promise((resolve) => {
+    setTimeout(resolve, 2000)
+  })  
+  return(
   <div>
-  <h1  className="border-4 border-stone-700 m-40 p-5 text-pink-600 font-bold text-center">Programming Page</h1>
+  <h1  className="border-4 border-stone-700 m-40 p-5 text-pink-600 font-bold text-center">About Page</h1>
   </div>
     )
   }
